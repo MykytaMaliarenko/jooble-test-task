e0f675b2baf6a2820889267fa96d80600099e064
mykyta <mykytamaliarenko@gmail.com>
2021-04-17 19:05:36 +0300

    add app.main.services.shortened_url_service

---
+++ app/main/service/shortened_url_service.py
@@ -0,0 +1,17 @@
+from datetime import timedelta
+from app.main.model.shortened_url import ShortenedUrl
+
+
+class ShortenedUrlService:
+
+    @staticmethod
+    def create(_id: str, url: str, ttl: timedelta) -> ShortenedUrl:
+        return ShortenedUrl(id=id, full_url=url, ttl=ttl)
+
+    @staticmethod
+    def has_id(_id: str) -> bool:
+        return ShortenedUrl.query.filter_by(id=_id).exists()
+
+    @staticmethod
+    def get_by_id(_id: str) -> ShortenedUrl:
+        return ShortenedUrl.query.filter_by(id=_id).first()
