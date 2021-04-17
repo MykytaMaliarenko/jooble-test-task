4d67d91cc46ea88ddb665d5553ae54fa2fd37cd5
mykyta <mykytamaliarenko@gmail.com>
2021-04-17 19:01:28 +0300

    ref app.main.urls: -rename to shortened_url -change type of ttl to Interval

---
+++ app/main/model/shortened_url.py
@@ -0,0 +1,13 @@
+from app import db
+from sqlalchemy import Column
+
+
+class ShortenedUrl(db.Model):
+    """ ShortenedUrl model for storing original url and ttl """
+
+    __tablename__ = "shortened_url"
+
+    id = db.Column(db.String(length=5), primary_key=True)
+    full_url = db.Column(db.String)
+    creation_date = Column(db.DateTime)
+    ttl = Column(db.Interval)
