13ac73cc7f4b4118f9c80561b050998cd9cf17a8
mykyta <mykytamaliarenko@gmail.com>
2021-04-19 01:47:31 +0300

    add celery beat

--- celery_worker.py
+++ celery_worker.py
@@ -2,11 +2,25 @@
 Run using the command:
 python celery -A celery_worker.celery worker -l info
 """
+from datetime import timedelta
+
 from dotenv import load_dotenv
 load_dotenv()
 
 from app import celery_app, create_app
 
 app = create_app()
+app.config['CELERYBEAT_SCHEDULE'] = {
+    # Executes every minute
+    'db-clearing': {
+        'task': 'app.main.tasks.delete_old_records',
+        'schedule': timedelta(minutes=1)
+    },
+    'id-generation': {
+        'task': 'app.main.tasks.generate_ids',
+        'schedule': timedelta(seconds=10)
+    }
+}
+
 celery = celery_app.create_celery_app(app)
 celery_app.celery = celery
