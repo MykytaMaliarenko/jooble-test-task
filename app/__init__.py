8e2b4ac6633e01a3d9f217f854159327bbf43f18
mykyta <mykytamaliarenko@gmail.com>
2021-04-18 22:33:57 +0300

    add redis

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,13 @@
+import os
+
+import redis
 from flask_sqlalchemy import SQLAlchemy
+
+
+def create_redis_instance():
+    return redis.Redis(host=os.getenv("REDIS_HOST"),
+                       db=os.getenv("REDIS_DB"),
+                       port=6379)
+
 
 db = SQLAlchemy()
