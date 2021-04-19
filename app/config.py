2d01c27913793d528828963a47f5fc339a83a295
mykyta <mykytamaliarenko@gmail.com>
2021-04-19 03:08:23 +0300

    small config changes

--- app/config.py
+++ app/config.py
@@ -6,29 +6,20 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig:
     SECRET_KEY = os.getenv('SECRET_KEY', 'super_secret_key')
     CELERY_BROKER_URL = os.getenv("CELERY_BROKER")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DB")
     DEBUG = True
 
 
 class DevelopmentConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DEV_DB")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class TestingConfig(BaseConfig):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DB")
-    PRESERVE_CONTEXT_ON_EXCEPTION = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.getenv("PROD_DB")
 
 
 configs = dict(
     dev=DevelopmentConfig,
-    test=TestingConfig,
     prod=ProductionConfig
 )
 
--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import unittest
 from dotenv import load_dotenv
 from flask_script import Manager
 
-from app import create_app
+from app import create_app, db
 
 # load env from .env in project root folder
 load_dotenv()
@@ -19,16 +19,6 @@ manager = Manager(app)
 @manager.command
 def run():
     app.run()
-
-
-@manager.command
-def test():
-    """Runs the unit tests."""
-    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
 
 
 if __name__ == '__main__':
